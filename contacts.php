f8f675176cb6cb12746f62f7eebe8cca5bffb384
iBreadd <124068827+iBreadd@users.noreply.github.com>
2023-11-02 12:44:08 +0200

    new

---
+++ contacts.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <nav>
+            <ul>
+                <li>
+                    <a href="/index.php">Начало</a>
+                </li>
+            </ul>
+        </nav>
+        Здравей :)
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <nav>
+            <ul>
+                <li>
+                    <a href="/contacts.php">Контакти</a>
+                </li>
+            </ul>
+        </nav>
+        Здравей :)
+    </body>
+</html>
